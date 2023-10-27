70cc4e2fb44705d9bb2349f26aac10e060e5b330
Lambang <gilbertrafdi@gmail.com>
2023-10-27 15:05:53 +0700

    test

--- sql_app/my_package/database.py
+++ sql_app/my_package/database.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://default:STgkOR1zx8sE@ep-sparkling-shadow-07727907-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
