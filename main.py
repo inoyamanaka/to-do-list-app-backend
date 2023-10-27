a806229dc78ff636f44eda1f9c3dad6598684c87
Lambang <gilbertrafdi@gmail.com>
2023-10-27 20:15:06 +0700

    hapus header barear

--- main.py
+++ main.py
@@ -144,7 +144,6 @@ async def login_for_access_token(
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
