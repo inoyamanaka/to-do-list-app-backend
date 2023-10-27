c4eeb3c7a472a96b395842787ca61d632281844e
Lambang <gilbertrafdi@gmail.com>
2023-10-27 15:00:04 +0700

    uploaddddd dbb cukk

--- sql_app/my_package/database.py
+++ sql_app/my_package/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "@vercel/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://default:STgkOR1zx8sE@ep-sparkling-shadow-07727907-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
